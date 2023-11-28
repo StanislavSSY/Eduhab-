826d1c73fb491708e0d46d9074c2918785f6abc2
Stanislav Sventskii <stanislav.mifub@yandex.ru>
2023-11-28 19:41:21 +0700

    find and best

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,16 @@ import "./App.css";
 import Auth from "./pages/Auth/Auth";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
+import OftenSearched from "./components/OftenSearched/OftenSearched";
+import FindCourse from "./components/FindCourse/FindCourse";
 
 function App(): JSX.Element {
   return (
     <>
       <Navbar />
       <Auth />
+     <FindCourse />
+      <OftenSearched />
       <Routes>
         <Route index element={<Main />} />
       </Routes>

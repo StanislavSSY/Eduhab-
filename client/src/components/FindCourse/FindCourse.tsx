c50b32cb7d4bd60470b4459191ff88b0465a4676
toPrecision <web.faste@gmail.com>
2023-11-28 16:01:22 +0300

    a

--- client/src/components/FindCourse/FindCourse.tsx
+++ client/src/components/FindCourse/FindCourse.tsx
@@ -1,50 +1,48 @@
 // FindCourse.tsx
-import React from 'react';
-import styles from './FindCourse.module.css';
+import React from "react";
+import styles from "./FindCourse.module.css";
 
 export default function FindCourse(): JSX.Element {
   return (
-    <div className={styles['search-form']}>
-      <div className={styles['search-form__form']}>
-        <div className={styles['search-form__input-wrapper']}>
-          <span className={styles['search-form__autocomplete']}>
-            <div className={styles['drop-down']}>
-              <div className={styles['with-autocomplete__drop-down']}>
-
-                <div className={styles['with-autocomplete__content']}>
+    <div className={styles["search-form"]}>
+      <div className={styles["search-form__form"]}>
+        <div className={styles["search-form__input-wrapper"]}>
+          <span className={styles["search-form__autocomplete"]}>
+            <div className={styles["drop-down"]}>
+              <div className={styles["with-autocomplete__drop-down"]}>
+                <div className={styles["with-autocomplete__content"]}>
                   <input
-                    className={styles['search-form__input']}
+                    className={styles["search-form__input"]}
                     placeholder="Название курса, автор или предмет"
                     autoComplete="off"
                     spellCheck="false"
                     aria-label="Search"
                     type="search"
                   />
-
-                 
-
                 </div>
-
               </div>
             </div>
           </span>
         </div>
 
-
-        <label className={styles['form-checkbox']}>
+        <label className={styles["form-checkbox"]}>
           <input type="checkbox" />
           <span>С сертификатами</span>
         </label>
 
-        <label className={styles['form-checkbox']}>
+        <label className={styles["form-checkbox"]}>
           <input type="checkbox" />
           <span>Бесплатные</span>
         </label>
 
-        <button className={`${styles['button_with-loader']} ${styles['search-form__submit']}`} type="button" data-ember-action="" data-ember-action-345="345">
+        <button
+          className={`${styles["button_with-loader"]} ${styles["search-form__submit"]}`}
+          type="button"
+          data-ember-action=""
+          data-ember-action-345="345"
+        >
           Искать
         </button>
-        
       </div>
     </div>
   );

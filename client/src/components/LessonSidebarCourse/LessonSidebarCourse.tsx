3aa99ad67c5ac89f3ad580f3af840b08ce92cbfa
toPrecision <web.faste@gmail.com>
2023-11-30 21:27:28 +0300

    re

--- client/src/components/LessonSidebarCourse/LessonSidebarCourse.tsx
+++ client/src/components/LessonSidebarCourse/LessonSidebarCourse.tsx
@@ -1,18 +1,14 @@
 import React, { useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  Outlet,
-  useParams,
-} from "react-router-dom";
+import { Link, Route } from "react-router-dom"; // Импорт компонента Link
 import styles from "./LessonSidebarCourse.module.css";
-
+import LessonContent from "../LessonContent/LessonContent";
 // /lesson/:lessonid/step/1
+
+// ... (ваш импорт и стиль)
 
 const LessonSidebarCourse = () => {
   const [selectedMenuItem, setSelectedMenuItem] = useState(null);
+  const [selectedStep, setSelectedStep] = useState(null);
 
   const menuItems = [
     {
@@ -22,17 +18,50 @@ const LessonSidebarCourse = () => {
         {
           id: 1,
           title: "Введение в программирование",
-          content: "Содержание введения в программирование...",
+          steps: [
+            {
+              id: 1,
+              type: "TEXT",
+              data: "какая-то дата",
+            },
+            {
+              id: 2,
+              type: "TEXT",
+              data: "какая-то дата2",
+            },
+          ],
         },
         {
           id: 2,
           title: "Основы JavaScript",
-          content: "Содержание основ JavaScript...",
+          steps: [
+            {
+              id: 1,
+              type: "TEXT",
+              data: "какая-то дата3",
+            },
+            {
+              id: 2,
+              type: "TEXT",
+              data: "какая-то дата4",
+            },
+          ],
         },
         {
           id: 3,
           title: "Работа с React",
-          content: "Содержание работы с React...",
+          steps: [
+            {
+              id: 1,
+              type: "TEXT",
+              data: "какая-то дата5",
+            },
+            {
+              id: 2,
+              type: "TEXT",
+              data: "какая-то дат6",
+            },
+          ],
         },
       ],
     },
@@ -43,17 +72,50 @@ const LessonSidebarCourse = () => {
         {
           id: 4,
           title: "Введение в программирование",
-          content: "Содержание введения в программирование...",
+          steps: [
+            {
+              id: 1,
+              type: "TEXT",
+              data: "какая-то дата6",
+            },
+            {
+              id: 2,
+              type: "TEXT",
+              data: "какая-то дат7",
+            },
+          ],
         },
         {
           id: 5,
           title: "Основы JavaScript",
-          content: "Содержание основ JavaScript...",
+          steps: [
+            {
+              id: 1,
+              type: "TEXT",
+              data: "какая-то дата8",
+            },
+            {
+              id: 2,
+              type: "TEXT",
+              data: "какая-то дат9",
+            },
+          ],
         },
         {
           id: 6,
           title: "Работа с React",
-          content: "Содержание работы с React...Содержание работы с React...",
+          steps: [
+            {
+              id: 1,
+              type: "TEXT",
+              data: "какая-то дата10",
+            },
+            {
+              id: 2,
+              type: "TEXT",
+              data: "какая-то дат10",
+            },
+          ],
         },
       ],
     },
@@ -61,6 +123,12 @@ const LessonSidebarCourse = () => {
 
   const handleMenuItemClick = (id) => {
     setSelectedMenuItem(id);
+    // Сбросить выбранный шаг при изменении урока
+    setSelectedStep(null);
+  };
+
+  const handleStepClick = (stepId) => {
+    setSelectedStep(stepId);
   };
 
   return (
@@ -79,7 +147,11 @@ const LessonSidebarCourse = () => {
                     onClick={() => handleMenuItemClick(lesson.id)}
                     className={selectedMenuItem === lesson.id ? "active" : ""}
                   >
-                    <Link to={`${lesson.id}/step/1`}>{lesson.title}</Link>
+                    <Link
+                      to={`/teach/courses/lesson/${lesson.id}/step/${lesson.steps[0].id}`}
+                    >
+                      {lesson.title}
+                    </Link>
                   </li>
                 ))}
               </ul>
@@ -100,13 +172,35 @@ const LessonSidebarCourse = () => {
                   .find((lesson) => lesson.id === selectedMenuItem).title
               }
             </h2>
-            <p>
-              {
-                menuItems
-                  .flatMap((module) => module.lesson)
-                  .find((lesson) => lesson.id === selectedMenuItem).content
-              }
-            </p>
+            {selectedStep !== null ? (
+              <div>
+                <h3>Шаг {selectedStep}</h3>
+                <p>
+                  {
+                    menuItems
+                      .flatMap((module) => module.lesson)
+                      .find((lesson) => lesson.id === selectedMenuItem)
+                      .steps.find((step) => step.id === selectedStep).data
+                  }
+                </p>
+              </div>
+            ) : (
+              <div>
+                <h3>Выберите шаг для просмотра содержания.</h3>
+                <ul className={styles.stepButtons}>
+                  {menuItems
+                    .flatMap((module) => module.lesson)
+                    .find((lesson) => lesson.id === selectedMenuItem)
+                    .steps.map((step) => (
+                      <li key={step.id}>
+                        <button onClick={() => handleStepClick(step.id)}>
+                          Шаг {step.id}
+                        </button>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            )}
           </div>
         ) : (
           <p>Выберите элемент меню для просмотра содержания.</p>

5073609227fd916a9cb2ca4c785214e0aeceb0a1
Ftulka <rizo.ra@yandex.ru>
2023-11-30 10:19:52 +0400

    small fixes

--- server/routes/entries.js
+++ server/routes/entries.js
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
           progress.push({
             id: step.id,
             type: step.type,
-            statusOrAnswer: false,
+            status: false,
           });
         });
       });

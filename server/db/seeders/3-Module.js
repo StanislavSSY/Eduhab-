c7863d1e0b32ed145d7f1d0ef95b11a8a72653bb
toPrecision <web.faste@gmail.com>
2023-12-08 14:19:59 +0300

    fgfgf

--- server/db/seeders/3-Module.js
+++ server/db/seeders/3-Module.js
@@ -1,56 +1,74 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      'Modules',
+      "Modules",
       [
         {
           courseid: 1,
-          title: 'Простейшие теги',
+          title: "Простейшие теги",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 1,
-          title: 'Сложные теги',
+          title: "Сложные теги",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 2,
-          title: 'Введение в основы',
+          title: "Введение в основы",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 2,
-          title: 'Функции, структуры, указатели и другое',
+          title: "Функции, структуры, указатели и другое",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 3,
-          title: 'Wprowadzenie do React.js',
+          title: "Wprowadzenie do React.js",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 3,
-          title: 'JSX oraz Next.js jako rdzeń projektu w React',
+          title: "JSX oraz Next.js jako rdzeń projektu w React",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 4,
-          title: 'Вводный курс',
+          title: "Вводный курс",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
           courseid: 4,
-          title: 'Основной курс',
+          title: "Основной курс",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          courseid: 7,
+          title: "Основы JavaScript",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          courseid: 7,
+          title: "Функции и Объекты",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          courseid: 7,
+          title: "Взаимодействие с Веб-страницей",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -60,6 +78,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Modules', null, {});
+    await queryInterface.bulkDelete("Modules", null, {});
   },
 };
